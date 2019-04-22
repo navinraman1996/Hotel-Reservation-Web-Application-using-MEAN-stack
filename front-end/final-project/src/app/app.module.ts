2c77173b242da870fea0160122ef444f2b95bf0d
Navin Raman <raman.na@husky.neu.edu>
2019-04-22 13:48:17 -0400

    front end 30 percent

--- front-end/final-project/src/app/app.module.ts
+++ front-end/final-project/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './navigation/header/header.component';
+
+import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { SearchComponent } from './search/search.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    HeaderComponent,
+    NavtabsComponent,
+    SidenavListComponent,
+    SearchComponent,
+    AccomodationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/final-project/src/app/material.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import {
+  MatBadgeModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatTabsModule,
+  MatGridListModule,
+  MatCardModule
+} from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule
+  ],
+  exports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule
+  ]
+})
+
+export class MaterialModule {}
